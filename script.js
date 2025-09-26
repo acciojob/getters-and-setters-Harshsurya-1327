b1440b8a0f2c7184f80c91a7791c6063e580f680
Harshsurya-1327 <harshsurya1327@gmail.com>
2025-09-26 23:35:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,45 +1,85 @@
-//complete this code
 class Person {
-	constructor(name,age){
-		this.name=name;
-		this.age=age;
-	}
+  constructor(name, age) {
+    this.name = name;
+    this._age = age;
+  }
 
-	get getNAme(name){
-		return this.name;
-	}
+  get age() {
+    return this._age;
+  }
 
-	set setName(age){
-		this.age=age;
-	}
+  set setAge(newAge) {
+    this._age = newAge;
+  }
 }
 
 class Student extends Person {
-	study(){
-		console.log(this.name,"is studying");
-	}
+  study() {
+    console.log(`${this.name} is studying`);
+  }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(this.name."is teaching");
-	}
+  teach() {
+    console.log(this.name, "is teaching");
+  }
 }
 
 // Do not change the code below this line
 const person = new Person("John", 25);
 console.log(person.name);
 
-person.age = 30;  // Using the setter to change the age
+person.setAge = 30;  // Using the setter to change the age
 console.log(person.age);  // Output: 30
 
 const student = new Student("Alice", 22);
 student.study();
 
 const teacher = new Teacher("Bob", 40);
-teacher.teach(); 
-
+teacher.teach();
 
 window.Person = Person;
 window.Student = Student;
-window.Teacher = Teacher;
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this._age = age;
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set setAge(newAge) {
+    this._age = newAge;
+  }
+}
+
+class Student extends Person {
+  study() {
+    console.log(`${this.name} is studying`);
+  }
+}
+
+class Teacher extends Person {
+  teach() {
+    console.log(this.name, "is teaching");
+  }
+}
+
+// Do not change the code below this line
+const person = new Person("John", 25);
+console.log(person.name);
+
+person.setAge = 30;  // Using the setter to change the age
+console.log(person.age);  // Output: 30
+
+const student = new Student("Alice", 22);
+student.study();
+
+const teacher = new Teacher("Bob", 40);
+teacher.teach();
+
+window.Person = Person;
+window.Student = Student;
+window.Teacher=Teacher;
