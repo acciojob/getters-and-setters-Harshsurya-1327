4f6753be684255fe709713d162ebb4196656bdb0
Harshsurya-1327 <harshsurya1327@gmail.com>
2025-09-26 23:46:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 class Person {
   constructor(name, age) {
     this.name = name;
@@ -21,7 +22,7 @@ class Student extends Person {
 
 class Teacher extends Person {
   teach() {
-    console.log(this.name, "is teaching");
+    console.log(`${this.name} is teaching`);
   }
 }
 
@@ -35,50 +36,15 @@ console.log(person.age);  // Output: 30
 const student = new Student("Alice", 22);
 student.study();
 
-const teacher = new Teacher("Bob", 40);
+const student1 = new Student("John", 22);
+student1.study();
+
+
+const teacher = new Teacher("Alice", 40);
 teacher.teach();
 
-window.Person = Person;
-window.Student = Student;
-class Person {
-  constructor(name, age) {
-    this.name = name;
-    this._age = age;
-  }
-
-  get age() {
-    return this._age;
-  }
-
-  set setAge(newAge) {
-    this._age = newAge;
-  }
-}
-
-class Student extends Person {
-  study() {
-    console.log(`${this.name} is studying`);
-  }
-}
-
-class Teacher extends Person {
-  teach() {
-    console.log(this.name, "is teaching");
-  }
-}
-
-// Do not change the code below this line
-const person = new Person("John", 25);
-console.log(person.name);
-
-person.setAge = 30;  // Using the setter to change the age
-console.log(person.age);  // Output: 30
-
-const student = new Student("Alice", 22);
-student.study();
-
-const teacher = new Teacher("Bob", 40);
-teacher.teach();
+const teacher1 = new Teacher("John", 40);
+teacher1.teach();
 
 window.Person = Person;
 window.Student = Student;
